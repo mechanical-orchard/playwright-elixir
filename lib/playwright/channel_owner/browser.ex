eff01c0b9606780bd76fc0afed1efec9c4def6fe
Eric Saxby <sax@geometer.io>
2021-06-08 20:23:19 -0700

    Match on required return values when opening a new Browser

--- lib/playwright/channel_owner/browser.ex
+++ lib/playwright/channel_owner/browser.ex
@@ -1,6 +1,7 @@
 defmodule Playwright.ChannelOwner.Browser do
   @moduledoc false
   use Playwright.ChannelOwner
+  alias Playwright.ChannelOwner
 
   # API
   # ---------------------------------------------------------------------------
@@ -10,12 +11,26 @@ defmodule Playwright.ChannelOwner.Browser do
   end
 
   def new_context(channel_owner) do
-    Channel.send(channel_owner, "newContext", %{noDefaultViewport: false, sdkLanguage: "elixir"})
+    context = Channel.send(channel_owner, "newContext", %{noDefaultViewport: false, sdkLanguage: "elixir"})
+
+    case context do
+      %ChannelOwner.BrowserContext{} ->
+        context
+
+      _other ->
+        raise("expected new_context to return a Playwright.ChannelOwner.BrowserContext, received: #{inspect(context)}")
+    end
   end
 
+  @spec new_page(ChannelOwner.Browser.t()) :: ChannelOwner.Page.t()
   def new_page(channel_owner) do
     context = new_context(channel_owner)
-    BrowserContext.new_page(context, %{owned_context: context})
+    page = BrowserContext.new_page(context, %{owned_context: context})
+
+    case page do
+      %ChannelOwner.Page{} -> page
+      _other -> raise("expected new_page to return a Playwright.ChannelOwner.Page, received: #{inspect(page)}")
+    end
   end
 
   def contexts(channel_owner) do
