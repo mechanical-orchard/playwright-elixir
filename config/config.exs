23491209e4b72084ce2a70c9d9acd0d8e7cd9b7d
Erik Hanson <erik@eahanson.com>
2021-06-14 11:39:29 -0700

    Add config to enable running of the asset server
    Co-authored-by: Corey Innis <coreyti@users.noreply.github.com>

--- config/config.exs
+++ config/config.exs
@@ -3,11 +3,13 @@ import Config
 endpoint = System.get_env("PLAYWRIGHT_ENDPOINT", "ws://localhost:3000/playwright")
 headless = String.to_atom(System.get_env("PLAYWRIGHT_HEADLESS", "true")) != false
 transport = String.to_atom(System.get_env("PLAYWRIGHT_TRANSPORT", "driver"))
+run_asset_server = String.to_atom(System.get_env("PLAYWRIGHT_RUN_ASSET_SERVER", to_string(transport == :driver)))
 
 config :playwright,
   endpoint: endpoint,
   headless: headless,
-  transport: transport
+  transport: transport,
+  run_asset_server: run_asset_server
 
 if config_env() == :test do
   config :logger, level: :info
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,2 +1,4 @@
-# {:ok, _} = Test.Support.AssetsServer.start(nil, nil)
+if Application.get_env(:playwright, :run_asset_server),
+  do: {:ok, _} = Test.Support.AssetsServer.start(nil, nil)
+
 ExUnit.start()
