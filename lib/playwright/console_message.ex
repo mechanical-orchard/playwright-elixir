938ce9e67acc8f1444cac964eecff57732e98f51
Corey Innis <coreyti@me.com>
2021-11-21 20:00:56 -0800

    Add docs, specs, etc. to `ConsoleMessage`

--- lib/playwright/console_message.ex
+++ lib/playwright/console_message.ex
@@ -1,7 +1,10 @@
 defmodule Playwright.ConsoleMessage do
-  @moduledoc false
+  @moduledoc """
+  `Playwright.ConsoleMessage` instances are dispatched by page and handled via
+  `Playwright.Page.on/3` for the `:console` event type.
+  """
   use Playwright.ChannelOwner, fields: [:message_text, :message_type]
-  alias Playwright.ChannelOwner
+  alias Playwright.{ChannelOwner}
 
   # callbacks
   # ---------------------------------------------------------------------------
@@ -10,4 +13,26 @@ defmodule Playwright.ConsoleMessage do
   def init(owner, initializer) do
     {:ok, %{owner | message_text: initializer.text, message_type: initializer.type}}
   end
+
+  # API
+  # ---------------------------------------------------------------------------
+
+  # ---
+
+  # @spec args(ConsoleMessage.t()) :: [JSHandle.t()]
+  # def args(owner)
+
+  # @spec location(ConsoleMessage.t()) :: call_location()
+  # def location(owner)
+
+  # @spec location(ConsoleMessage.t()) :: call_location()
+  # def location(owner)
+
+  # @spec text(ConsoleMessage.t()) :: String.t()
+  # def text(owner)
+
+  # @spec type(ConsoleMessage.t()) :: String.t()
+  # def type(owner)
+
+  # ---
 end
