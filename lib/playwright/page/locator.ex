4d3ae81955d5549abf9b8820982b2916be9502d2
Eric Saxby <sax@geometer.io>
2021-11-23 08:53:49 -0800

    Add doc to Page.Locator

--- lib/playwright/page/locator.ex
+++ lib/playwright/page/locator.ex
@@ -1,6 +1,16 @@
 defmodule Playwright.Page.Locator do
+  @moduledoc """
+  `Playwright.Page.Locator` wraps `Playwright.Frame.Locator`, using the main
+  frame of the page.
+  """
   alias Playwright.{Frame, Page}
 
+  @doc """
+  Returns an element locator that can be used to perform actions on the page.
+  This locator is resolved immediately before performing an action, so a series
+  of actions on the same locator can in fact be performed on different DOM
+  elements, as may happen when the DOM structure changes between actions.
+  """
   def new(page, selector) do
     Frame.Locator.new(Page.main_frame(page), selector)
   end
