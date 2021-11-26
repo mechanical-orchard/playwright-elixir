fd102006a6dcd50444ffd7f7761e68d9e4c32589
Corey Innis <coreyti@me.com>
2021-11-26 09:21:49 -0800

    Add placeholder specs and fn heads for `Worker`

--- lib/playwright/worker.ex
+++ lib/playwright/worker.ex
@@ -1,4 +1,10 @@
 defmodule Playwright.Worker do
-  @moduledoc "..."
+  @moduledoc """
+  ...
+  """
   use Playwright.ChannelOwner
+
+  # @spec expect_event(t() | {:ok, t()}, binary(), function(), options()) :: {:ok, map()}
+  # def expect_event(worker, event, predicate \\ nil, options \\ %{})
+  # ...delegate wait_for_event -> expect_event
 end
