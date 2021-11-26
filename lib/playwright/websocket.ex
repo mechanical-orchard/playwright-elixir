c0376d92fb1dd97e286ac7df2a4545e5d1b83c71
Corey Innis <coreyti@me.com>
2021-11-26 09:19:56 -0800

    Add placeholder specs and fn heads for `WebSocket`

--- lib/playwright/websocket.ex
+++ lib/playwright/websocket.ex
@@ -1,4 +1,20 @@
 defmodule Playwright.WebSocket do
-  @moduledoc false
+  @moduledoc """
+  ...
+  """
   use Playwright.ChannelOwner
+
+  @property :is_closed
+  @property :url
+
+  # ---
+
+  # @spec expect_event(t() | {:ok, t()}, binary(), function(), options()) :: {:ok, map()}
+  # def expect_event(web_socket, event, predicate \\ nil, options \\ %{})
+  # ...delegate wait_for_event -> expect_event
+
+  # @spec on(t() | {:ok, t()}, binary(), function()) :: nil
+  # def on(web_socket, event, callback)
+
+  # ---
 end
