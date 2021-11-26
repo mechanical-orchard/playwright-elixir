fa1c510c6ebe54c8d91b0d1d0a78bfc5852da485
Corey Innis <coreyti@me.com>
2021-11-26 08:54:19 -0800

    Add placeholder specs and fn heads for `Response`

--- lib/playwright/response.ex
+++ lib/playwright/response.ex
@@ -3,34 +3,71 @@ defmodule Playwright.Response do
   use Playwright.ChannelOwner
   alias Playwright.Response
 
+  @property :frame
   @property :headers
+  @property :request
   @property :status
+  @property :status_text
   @property :url
-
-  # derived from :initializer
-  # ---------------------------------------------------------------------------
-
-  @spec ok(Response.t()) :: boolean()
-  def ok(%Response{} = response) do
-    response.status === 0 || (response.status >= 200 && response.status <= 299)
-  end
-
-  @doc false
-  def ok({:ok, response}) do
-    ok(response)
-  end
 
   # API call
   # ---------------------------------------------------------------------------
 
-  @spec body(Response.t()) :: {:ok, binary()}
+  # ---
+
+  # @spec all_headers(Response.t()) :: {:ok, map()}
+  # def all_headers(response)
+
+  # ---
+
+  @spec body(t() | {:ok, t()}) :: {:ok, binary()}
   def body(%Response{} = response) do
     {:ok, result} = Channel.post(response, :body)
     Base.decode64(result)
   end
 
-  @doc false
   def body({:ok, response}) do
     body(response)
   end
+
+  # ---
+
+  # @spec finished(Response.t()) :: :ok | {:error, SomeError.t()}
+  # def finished(response)
+
+  # @spec header_value(Response.t(), binary()) :: {:ok, binary() | nil}
+  # def header_value(response, name)
+
+  # @spec header_values(Response.t()) :: {:ok, [binary()]}
+  # def header_values(response)
+
+  # @spec headers_array(Response.t()) :: {:ok, [map()]}
+  # def headers_array(response)
+
+  # @spec json(Response.t()) :: {:ok, Serializable.t()}
+  # def json(response)
+
+  # ---
+
+  @spec ok(t() | {:ok, t()}) :: boolean()
+  def ok(%Response{} = response) do
+    response.status === 0 || (response.status >= 200 && response.status <= 299)
+  end
+
+  def ok({:ok, response}) do
+    ok(response)
+  end
+
+  # ---
+
+  # @spec security_details(Response.t()) :: {:ok, map() | nil)}
+  # def security_details(response)
+
+  # @spec server_addr(Response.t()) :: {:ok, map() | nil)}
+  # def server_addr(response)
+
+  # @spec text(Response.t()) :: {:ok, binary()}
+  # def text(response)
+
+  # ---
 end
