d2ea6be680180ee81f4116789ecfeb545be621fa
Corey Innis <corey@geometer.io>
2021-12-02 13:46:15 -0800

    Add test for using `BrowserContext` with a proxy

---
+++ test/integration/browser_context/proxy_test.exs
@@ -0,0 +1,25 @@
+defmodule Playwright.BrowserContext.ProxyTest do
+  use Playwright.TestCase
+  alias Playwright.{Browser, BrowserContext, Page, Response}
+
+  describe "BrowserContext created with a proxy" do
+    @tag exclude: [:page]
+    test "uses the proxy", %{assets: assets, browser: browser} do
+      context =
+        Browser.new_context(browser, %{
+          proxy: %{server: assets.prefix}
+        })
+
+      {:ok, page} = BrowserContext.new_page(context)
+
+      {:ok, response} = Page.goto(page, "http://non-existent.com/dom.html")
+      assert Response.ok(response)
+      assert {:ok, "Text,\nmore text"} = Page.text_content(page, "#inner")
+    end
+
+    # test_should_use_proxy_for_second_page
+    # test_should_work_with_ip_port_notion
+    # test_should_authenticate
+    # test_should_authenticate_with_empty_password
+  end
+end
