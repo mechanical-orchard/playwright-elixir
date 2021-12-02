f6cf3e83df53e9e1ca970efce05cd2da516a6368
Greg Woodward <woodward@alumni.stanford.edu>
2021-12-01 16:40:29 -0800

    Use the LaunchOption playwright_cli_path

--- lib/playwright/cli.ex
+++ lib/playwright/cli.ex
@@ -6,9 +6,7 @@ defmodule Playwright.CLI do
   require Logger
 
   def install_browsers do
-    default_cli_path = Path.expand("../../priv/static/playwright_cli.js", __DIR__)
-    cli_path = Application.get_env(:playwright, ConnectOptions) |> Keyword.get(:playwright_cli_path, default_cli_path)
-
+    cli_path = Application.get_env(:playwright, LaunchOptions)[:playwright_cli_path]
     {output_lines, 0} = System.cmd(cli_path, ["install"])
 
     output_lines
