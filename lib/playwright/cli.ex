b06b68bf88d7148caf68c4a9d3d2c6b361fa34c3
Corey Innis <corey@geometer.io>
2022-01-13 13:34:28 -0800

    FIX: `mix playwright.install_browsers` w/out config
    it was blowing up trying to find the CLI path if that was not in the application configuration.

--- lib/playwright/cli.ex
+++ lib/playwright/cli.ex
@@ -7,9 +7,20 @@ defmodule Playwright.CLI do
 
   def install_browsers do
     Logger.info("Installing playwright browsers")
-    cli_path = Application.get_env(:playwright, LaunchOptions)[:playwright_cli_path]
+    cli_path = config_cli() || default_cli()
     {result, exit_status} = System.cmd(cli_path, ["install"])
     Logger.info(result)
     if exit_status != 0, do: raise("Failed to install playwright browsers")
+  end
+
+  # private
+  # ----------------------------------------------------------------------------
+
+  defp config_cli do
+    Application.get_env(:playwright, LaunchOptions)[:playwright_cli_path]
+  end
+
+  defp default_cli do
+    Path.join(:code.priv_dir(:playwright), "static/playwright_cli.js")
   end
 end
