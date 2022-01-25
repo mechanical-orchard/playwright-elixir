ce539c727a73c54fd213efa71e283ec888ce3f95
Jason Carter <jason@geometer.io>
2022-01-24 17:59:28 -0800

    Add mix task to run playwright install_deps via the CLI
    Co-authored-by: Corey Innis <corey@geometer.io>

--- lib/playwright/cli.ex
+++ lib/playwright/cli.ex
@@ -13,6 +13,14 @@ defmodule Playwright.CLI do
     if exit_status != 0, do: raise("Failed to install playwright browsers")
   end
 
+  def install_deps do
+    Logger.info("Installing playwright deps")
+    cli_path = config_cli() || default_cli()
+    {result, exit_status} = System.cmd(cli_path, ["install-deps"])
+    Logger.info(result)
+    if exit_status != 0, do: raise("Failed to install playwright deps")
+  end
+
   # private
   # ----------------------------------------------------------------------------
 
