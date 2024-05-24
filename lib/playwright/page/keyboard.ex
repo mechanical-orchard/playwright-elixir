207c8d1930fd6ed70404bd18a96d7f720dcdd4b0
Corey Innis <corey@mechanical-orchard.com>
2024-05-24 15:26:03 -0700

    Update keyboard.ex w/ placeholders

--- lib/playwright/page/keyboard.ex
+++ lib/playwright/page/keyboard.ex
@@ -1,3 +1,19 @@
 defmodule Playwright.Page.Keyboard do
   @moduledoc false
+
+  # @spec down(t(), binary()) :: :ok
+  # def down(page, key)
+
+  # @spec insert_text(t(), binary()) :: :ok
+  # def insert_text(page, text)
+
+  # @spec press(t(), binary(), options()) :: :ok
+  # def press(page, key, options \\ %{})
+
+  # @spec type(t(), binary(), options()) :: :ok
+  # def type(page, text, options \\ %{})
+
+  # @spec up(t(), binary()) :: :ok
+  # def up(page, key)
+
 end
