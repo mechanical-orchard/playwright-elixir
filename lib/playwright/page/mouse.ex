366b07f4aeee8d162de173f622dd7f9ace1efba3
Corey Innis <corey@mechanical-orchard.com>
2024-05-24 16:10:27 -0700

    Update mouse.ex w/ placeholders

--- lib/playwright/page/mouse.ex
+++ lib/playwright/page/mouse.ex
@@ -1,3 +1,22 @@
 defmodule Playwright.Page.Mouse do
   @moduledoc false
+
+  # @spec click(t(), number(), number(), options()) :: :ok
+  # def click(page, x, y, options \\ %{})
+
+  # @spec dblclick(t(), number(), number(), options()) :: :ok
+  # def dblclick(page, x, y, options \\ %{})
+
+  # @spec down(t(), options()) :: :ok
+  # def down(page, options \\ %{})
+
+  # @spec move(t(), number(), number(), options()) :: :ok
+  # def move(page, x, y, options \\ %{})
+
+  # @spec up(t(), options()) :: :ok
+  # def up(page, options \\ %{})
+
+  # @spec wheel(t(), number(), number()) :: :ok
+  # def wheel(page, delta_x, delta_y)
+
 end
