45efdcd29c8588a7c9e063e1cf7debc308c784f9
Corey Innis <corey@mechanical-orchard.com>
2024-05-24 15:32:12 -0700

    Update coverage.ex w/ placeholders

--- lib/playwright/page/coverage.ex
+++ lib/playwright/page/coverage.ex
@@ -1,3 +1,16 @@
 defmodule Playwright.Page.Coverage do
   @moduledoc false
+
+  # @spec start_css_coverage(t(), options()) :: :ok
+  # def start_css_coverage(page, options \\ %{})
+
+  # @spec start_js_coverage(t(), options()) :: :ok
+  # def start_js_coverage(page, options \\ %{})
+
+  # @spec stop_css_coverage(t()) :: result()
+  # def stop_css_coverage(page)
+
+  # @spec stop_js_coverage(t()) :: result()
+  # def stop_js_coverage(page)
+
 end
