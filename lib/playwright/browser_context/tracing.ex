fc9b18b911751ac787eed044356aa0dc9782861a
Corey Innis <corey@mechanical-orchard.com>
2024-05-25 11:29:28 -0700

    Update tracing.ex w/ placeholders

--- lib/playwright/browser_context/tracing.ex
+++ lib/playwright/browser_context/tracing.ex
@@ -1,3 +1,15 @@
 defmodule Playwright.BrowserContext.Tracing do
   @moduledoc false
+
+  # @spec start(Tracing.t(), options()) :: :ok
+  # def start(tracing, options \\ %{})
+
+  # @spec start_chunk(Tracing.t(), options()) :: :ok
+  # def start_chunk(tracing, options \\ %{})
+
+  # @spec stop(Tracing.t(), options()) :: :ok
+  # def stop(tracing, options \\ %{})
+
+  # @spec stop_chunck(Tracing.t(), options()) :: :ok
+  # def stop_chunck(tracing, options \\ %{})
 end
