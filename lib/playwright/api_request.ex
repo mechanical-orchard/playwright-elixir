1132909a65f7dc1e95cb50bb34e8752f504fdb0d
Corey Innis <corey@mechanical-orchard.com>
2024-05-24 18:04:57 -0700

    Update api_request.ex w/ placeholders

--- lib/playwright/api_request.ex
+++ lib/playwright/api_request.ex
@@ -1,4 +1,8 @@
 defmodule Playwright.APIRequest do
   @moduledoc false
   use Playwright.ChannelOwner
+
+  # @spec new_context(t(), options()) :: APIRequestContext.t()
+  # def new_context(api_request, options \\ %{})
+
 end
