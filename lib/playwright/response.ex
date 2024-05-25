a6293a2dcc9c74f3c6bb3c77859926f61a89b1d3
Corey Innis <corey@mechanical-orchard.com>
2024-05-25 11:14:02 -0700

    Update response.ex w/ placeholders

--- lib/playwright/response.ex
+++ lib/playwright/response.ex
@@ -33,11 +33,20 @@ defmodule Playwright.Response do
   # @spec finished(t()) :: :ok | {:error, SomeError.t()}
   # def finished(response)
 
+  # @spec frame(Response.t()) :: Frame.t()
+  # def frame(response)
+
+  # @spec from_service_worker(Response.t()) :: boolean()
+  # def from_service_worker(response)
+
   # @spec header_value(t(), binary()) :: binary() | nil
   # def header_value(response, name)
 
   # @spec header_values(t()) :: [binary()]
   # def header_values(response)
+
+  # @spec headers(Response.t()) :: headers() # map()
+  # def headers(response)
 
   # @spec headers_list(Response.t()) :: [map()]
   # def headers_list(response)
@@ -59,11 +68,20 @@ defmodule Playwright.Response do
 
   # ---
 
-  # @spec security_details(t()) :: map() | nil)
+  # @spec request(t()) :: Request.t()
+  # def request(response)
+
+  # @spec security_details(t()) :: map() | nil
   # def security_details(response)
 
-  # @spec server_addr(t()) :: map() | nil)
+  # @spec server_addr(t()) :: map() | nil
   # def server_addr(response)
+
+  # @spec status(t()) :: number()
+  # def status(response)
+
+  # @spec status_text(t()) :: binary()
+  # def status_text(response)
 
   # ---
 
@@ -71,4 +89,11 @@ defmodule Playwright.Response do
   def text(response) do
     body(response)
   end
+
+  # ---
+
+  # @spec url(t()) :: binary()
+  # def url(response)
+
+  # ---
 end
