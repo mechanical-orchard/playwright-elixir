e6af55069cbaafa5d84c3f55f63ffcdde26c58bf
Corey Innis <corey@mechanical-orchard.com>
2024-05-24 19:49:53 -0700

    Update dialog.ex w/ placeholders

--- lib/playwright/dialog.ex
+++ lib/playwright/dialog.ex
@@ -1,4 +1,23 @@
 defmodule Playwright.Dialog do
   @moduledoc false
   use Playwright.ChannelOwner
+
+  # @spec accept(Dialog.t(), binary()) :: :ok
+  # def accept(dialog, prompt \\ "")
+
+  # @spec default_value(Dialog.t()) :: binary()
+  # def default_value(dialog)
+
+  # @spec dismiss(Dialog.t()) :: :ok
+  # def dismiss(dialog)
+
+  # @spec message(Dialog.t()) :: binary()
+  # def message(dialog)
+
+  # @spec page(Dialog.t()) :: nil | Page.t()
+  # def page(dialog)
+
+  # @spec type(Dialog.t()) :: binary()
+  # def type(dialog)
+
 end
