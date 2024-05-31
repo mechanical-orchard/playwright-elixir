cea7272c07efa6020ae06f02cdf5aaec00b39771
Corey Innis <corey@mechanical-orchard.com>
2024-05-31 13:05:34 -0700

    API: fix specs for `Keyboard`

--- lib/playwright/page/keyboard.ex
+++ lib/playwright/page/keyboard.ex
@@ -7,27 +7,27 @@ defmodule Playwright.Page.Keyboard do
   # API
   # ---------------------------------------------------------------------------
 
-  @spec down(Page.t(), binary()) :: :ok
+  @spec down(Page.t(), binary()) :: Page.t()
   def down(page, key) do
     post!(page, :keyboard_down, %{key: key})
   end
 
-  @spec insert_text(Page.t(), binary()) :: :ok
+  @spec insert_text(Page.t(), binary()) :: Page.t()
   def insert_text(page, text) do
     post!(page, :keyboard_type, %{text: text})
   end
 
-  @spec press(Page.t(), binary()) :: :ok
+  @spec press(Page.t(), binary()) :: Page.t()
   def press(page, key) do
     post!(page, :keyboard_press, %{key: key})
   end
 
-  @spec type(Page.t(), binary()) :: :ok
+  @spec type(Page.t(), binary()) :: Page.t()
   def type(page, text) do
     post!(page, :keyboard_type, %{text: text})
   end
 
-  @spec up(Page.t(), binary()) :: :ok
+  @spec up(Page.t(), binary()) :: Page.t()
   def up(page, key) do
     post!(page, :keyboard_up, %{key: key})
   end
