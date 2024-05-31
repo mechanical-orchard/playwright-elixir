225a14350c1cdcad2076ca368a6c8504de19cb2b
Corey Innis <corey@mechanical-orchard.com>
2024-05-31 00:06:08 -0700

    Tidy: alpha-sort & group functions

--- lib/playwright/page/keyboard.ex
+++ lib/playwright/page/keyboard.ex
@@ -4,13 +4,26 @@ defmodule Playwright.Page.Keyboard do
   use Playwright.SDK.ChannelOwner
   alias Playwright.Page
 
-  @spec type(Page.t(), binary()) :: :ok
-  def type(page, text) do
-    channel_post(page, :keyboard_type, %{text: text})
+  # API
+  # ---------------------------------------------------------------------------
+
+  @spec down(Page.t(), binary()) :: :ok
+  def down(page, key) do
+    channel_post(page, :keyboard_down, %{key: key})
   end
 
   @spec insert_text(Page.t(), binary()) :: :ok
   def insert_text(page, text) do
+    channel_post(page, :keyboard_type, %{text: text})
+  end
+
+  @spec press(Page.t(), binary()) :: :ok
+  def press(page, key) do
+    channel_post(page, :keyboard_press, %{key: key})
+  end
+
+  @spec type(Page.t(), binary()) :: :ok
+  def type(page, text) do
     channel_post(page, :keyboard_type, %{text: text})
   end
 
@@ -19,15 +32,8 @@ defmodule Playwright.Page.Keyboard do
     channel_post(page, :keyboard_up, %{key: key})
   end
 
-  @spec down(Page.t(), binary()) :: :ok
-  def down(page, key) do
-    channel_post(page, :keyboard_down, %{key: key})
-  end
-
-  @spec press(Page.t(), binary()) :: :ok
-  def press(page, key) do
-    channel_post(page, :keyboard_press, %{key: key})
-  end
+  # private
+  # ---------------------------------------------------------------------------
 
   defp channel_post(%Page{} = page, action, data) do
     Channel.post(page.session, {:guid, page.guid}, action, data)
