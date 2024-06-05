ecad4712f34aa5eff3d7f9e836d95726f6b1ef82
Corey Innis <corey@mechanical-orchard.com>
2024-06-04 21:17:32 -0700

    API: `APIRequestContext` prepare docs/specs
    Co-authored-by: Ryan Spore <ryan@mechanical-orchard.com>

--- lib/playwright/api_request_context.ex
+++ lib/playwright/api_request_context.ex
@@ -1,7 +1,8 @@
 defmodule Playwright.APIRequestContext do
   @moduledoc """
-  This API is used for the Web API testing. You can use it to trigger API endpoints, configure micro-services,
-  prepare environment or the server to your e2e test.
+  This API is used for the Web API testing. You can use it to trigger API
+  endpoints, configure micro-services, prepare environment or the server to your
+  e2e test.
 
   Use this at caution as has not been tested.
 
@@ -10,23 +11,26 @@ defmodule Playwright.APIRequestContext do
   use Playwright.SDK.ChannelOwner
   alias Playwright.APIRequestContext
 
+  @typedoc "An explicit shorthand for the APIRequestContext.t() subject."
+  @type subject :: t()
+
   @type fetch_options() :: %{
           optional(:params) => any(),
           optional(:method) => binary(),
           optional(:headers) => any(),
-          optional(:postData) => any(),
-          optional(:jsonData) => any(),
-          optional(:formData) => any(),
-          optional(:multipartData) => any(),
+          optional(:post_data) => any(),
+          optional(:json_data) => any(),
+          optional(:form_data) => any(),
+          optional(:multipart_data) => any(),
           optional(:timeout) => non_neg_integer(),
-          optional(:failOnStatusCode) => boolean(),
-          optional(:ignoreHTTPSErrors) => boolean()
+          optional(:fail_on_status_code) => boolean(),
+          optional(:ignore_HTTPS_errors) => boolean()
         }
 
   # @spec delete(t(), binary(), options()) :: APIResponse.t()
   # def delete(context, url, options \\ %{})
 
-  # @spec dispose(t()) :: :ok
+  # @spec dispose(t()) :: subject()
   # def dispose(api_request_context)
 
   # @spec fetch(t(), binary() | Request.t(), options()) :: APIResponse.t()
