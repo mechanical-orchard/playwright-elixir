f424e2da5926c421e73f8740123c831c7f8eccad
Ryan Spore <ryan@mechanical-orchard.com>
2024-08-12 11:58:27 -0700

    Increase the error-handling timeout adjustment
    This attempts to ensure the browser server has time to timeout and communicate that timeout to our GenServer, before the GenServer call times out
    
    Co-authored-by: German Velasco <german@mechanical-orchard.com>
    Co-authored-by: Corey Innis <corey@mechanical-orchard.com>

--- lib/playwright/sdk/helpers/error_handling.ex
+++ lib/playwright/sdk/helpers/error_handling.ex
@@ -6,12 +6,13 @@ defmodule Playwright.SDK.Helpers.ErrorHandling do
     timeout = options |> Map.get(:timeout, 30_000)
 
     try do
-      # NOTE the HACK!
       # In most cases (as of 20240802), the timeout value provided here is also
       # used as a timeout option passed to the Playwright server. As such, there
       # is/was a race condition in which the `action` provided here would often
       # time out before a response from the server indicated it's own timeout.
-      action.(timeout + 5)
+      # To mitigate this, we add an additional 100ms to the timeout value to give
+      # the browser server a chance to timeout before the awaiting process times out.
+      action.(timeout + 100)
     catch
       :exit, {:timeout, _} = _reason ->
         {:error, Error.new(%{error: %{message: "Timeout #{inspect(timeout)}ms exceeded."}}, nil)}
